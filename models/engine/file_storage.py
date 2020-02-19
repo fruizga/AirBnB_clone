d8e21d26ec0ba4cbdeaf3b80b3302f6faf8e28e8
fruizga <ruizgallego@gmail.com>
2020-02-19 08:21:48 -0500

    upload file storge

---
+++ models/engine/file_storage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Store first object"""
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+
+
+class FileStorage:
+    """
+    serializes instances to a JSON file and
+    deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key obj.id"""
+        FileStorage.__objects[type(obj).__name__ + "." + obj.id] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        new_dict = {}
+        for key in FileStorage.__objects:
+            new_dict[key] = FileStorage.__objects[key].to_dict()
+        with open(FileStorage.__file_path, mode="w") as f:
+            json.dump(new_dict, f)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        try:
+            with open(FileStorage.__file_path, mode="r") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    FileStorage.__objects[key] = value
+        except Exception:
+            pass
+
+
+def delete_obj(self):
+    """Deletes all"""
+    FileStorage.__objects.clear()
